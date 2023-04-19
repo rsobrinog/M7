e2fee6849e914093c5f88c00904632bf482b431f
Roger <rsobrino@xtec.cat>
2023-04-19 17:36:32 +0200

    Soucionat error amb connector postgresql i railway des de main

--- Institut/Institut/settings.py
+++ Institut/Institut/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'Institut.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'aUUlQ40Y1Mh5Sv727Jfn',
+        'HOST':'Containers-us-west-7.railway.app',
+        'PORT':'5997',
     }
 }
 
--- Institut/alumnat/models.py
+++ Institut/alumnat/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Alumnat(models.Models):
+class Alumnat(models.Model):
     name = models.CharField(max_length=500)
     surname = models.CharField(max_length=500)
     age = models.CharField(max_length=500)
