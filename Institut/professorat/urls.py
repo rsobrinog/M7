3b61774a6a4507b925c22860d9c34b28aef3da6b
Roger <rsobrino@xtec.cat>
2023-04-19 20:09:37 +0200

    Afegit update i form a professorat

--- Institut/professorat/urls.py
+++ Institut/professorat/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('llista', views.teachers_list, name='teachers_list'),
     path('teacher-form/', views.teach_form, name='teach_form'),
+    path('update/<str:pk>/', views.update, name='update'),
 ]
